981b50293b2927a9ecd5c3d177b209b8391055ff
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-04-16 16:55:48 -0300

    criação da estrutura do backend

---
+++ fdBack/src/test/java/com/example/fd/firstdecision/FirstDecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fd.firstdecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDecisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
