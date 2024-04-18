2b9b0955ae1be96b1b2dd2440f34e031005d690b
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-04-18 10:26:02 -0300

    Testes iniciais da clienteContoller

--- fdBack/src/main/java/com/example/fd/firstdecision/repository/ClienteRepository.java
+++ fdBack/src/main/java/com/example/fd/firstdecision/repository/ClienteRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
     boolean existsByEmail(String email);
+
 }
--- fdBack/src/test/java/com/example/fd/firstdecision/controller/ClienteControllerTest.java
+++ fdBack/src/test/java/com/example/fd/firstdecision/controller/ClienteControllerTest.java
@@ -1,9 +1,80 @@
 package com.example.fd.firstdecision.controller;
 
+import com.example.fd.firstdecision.exception.EmailAlreadyExistsException;
 import com.example.fd.firstdecision.orm.Cliente;
+import com.example.fd.firstdecision.service.ClienteService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-public class ClienteController {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
+class ClienteControllerTest {
 
-    Cliente cliente = new Cliente();
+    @Mock
+    private ClienteService clienteService;
+
+    @InjectMocks
+    private ClienteController clienteController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testRegistrarCliente_EmailNaoExistente() {
+        Cliente cliente = new Cliente();
+        cliente.setEmail("test@example.com");
+
+        when(clienteService.validarSenha(cliente)).thenReturn(true);
+        doNothing().when(clienteService).existeEmail(cliente);
+        doNothing().when(clienteService).salvarCliente(cliente);
+
+        ResponseEntity<String> response = clienteController.registrarCliente(cliente);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Usuário registrado com sucesso.", response.getBody());
+        verify(clienteService, times(1)).validarSenha(cliente);
+        verify(clienteService, times(1)).existeEmail(cliente);
+        verify(clienteService, times(1)).salvarCliente(cliente);
+    }
+
+    @Test
+    void testRegistrarCliente_EmailExistente() {
+        Cliente cliente = new Cliente();
+        cliente.setEmail("test@example.com");
+
+        when(clienteService.validarSenha(cliente)).thenReturn(true);
+        doThrow(EmailAlreadyExistsException.class).when(clienteService).existeEmail(cliente);
+
+        ResponseEntity<String> response = clienteController.registrarCliente(cliente);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Este e-mail já está em uso.", response.getBody());
+        verify(clienteService, times(1)).validarSenha(cliente);
+        verify(clienteService, times(1)).existeEmail(cliente);
+        verify(clienteService, never()).salvarCliente(cliente);
+    }
+
+    @Test
+    void testRegistrarCliente_SenhaInvalida() {
+        Cliente cliente = new Cliente();
+        cliente.setEmail("test@example.com");
+
+        when(clienteService.validarSenha(cliente)).thenReturn(false);
+
+        ResponseEntity<String> response = clienteController.registrarCliente(cliente);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("A senha e a confirmação de senha não correspondem.", response.getBody());
+        verify(clienteService, times(1)).validarSenha(cliente);
+        verify(clienteService, never()).existeEmail(cliente);
+        verify(clienteService, never()).salvarCliente(cliente);
+    }
 }
