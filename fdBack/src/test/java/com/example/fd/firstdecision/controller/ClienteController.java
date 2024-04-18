a50707e8ae4a6e6e888a038b98701a53405b3334
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-04-18 06:17:44 -0300

    dependeẽncia e estrutura inicial dos testes em junit

---
+++ fdBack/src/test/java/com/example/fd/firstdecision/controller/ClienteController.java
@@ -0,0 +1,9 @@
+package com.example.fd.firstdecision.controller;
+
+import com.example.fd.firstdecision.orm.Cliente;
+
+public class ClienteController {
+
+
+    Cliente cliente = new Cliente();
+}
---
+++ fdBack/src/test/java/com/example/fd/firstdecision/service/ClienteServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.fd.firstdecision.service;
+
+import org.junit.Test;
+
+public class ClienteServiceTest {
+
+    @Test
+    public void teste() {
+
+    }
+}
